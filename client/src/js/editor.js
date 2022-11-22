886303ae539ac8f0ae7d9b21448574e170625860
Rjewell859 <66131189+Rjewell859@users.noreply.github.com>
2022-11-22 18:18:05 -0500

    Maybe this will fix the editor not covering page

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -24,8 +24,9 @@ export default class {
       autofocus: true,
       indentUnit: 2,
       tabSize: 2,
+      viewportMargin: Infinity
     });
-
+    
     // When the editor is ready, set the value to whatever is stored in indexeddb.
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
